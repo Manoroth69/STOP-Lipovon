dc66583a23455dbdd2bacdea53ce11107d382127
Apu <Manoroth69>
2025-06-05 00:44:32 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import { inject } from "@vercel/speed-insights"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 inject();
 
